1c387019c830ea62d0d67017e383de0e9e4fb06f
Alwin Tareen <altareen@gmail.com>
2015-10-08 16:24:13 +0800

    Added for loop examples

---
+++ 02Iteration/ForLoops.java
@@ -0,0 +1,10 @@
+public class ForLoops
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 22; i += 2)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ 02Iteration/Increment.java
@@ -0,0 +1,13 @@
+public class Increment
+{
+    public static void main(String[] args)
+    {
+        int x = 0;
+        x++;
+        x++;
+        System.out.println("x is " + x);
+        int y = 2;
+        y--;
+        System.out.println("y is " + y);
+    }
+}
