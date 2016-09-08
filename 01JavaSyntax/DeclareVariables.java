376ed05d26a5c0fd14849fa4afa182bb8b866126
Alwin Tareen <altareen@gmail.com>
2016-09-08 14:11:39 +0800

    Updated java files

---
+++ 01JavaSyntax/DeclareVariables.java
@@ -0,0 +1,25 @@
+public class DeclareVariables
+{
+    public static void main(String[] args)
+    {
+        // Declare variables
+        int distance;
+        double radius;
+        
+        // Initialize variables
+        distance = 875;
+        radius = 28.975;
+        
+        // Display values to the terminal
+        System.out.println(distance);
+        System.out.println(radius);
+        
+        // Update variables
+        distance = 1025;
+        radius = 32.625;
+        
+        // Display values to the terminal
+        System.out.println(distance);
+        System.out.println(radius);
+    }
+}
---
+++ 01JavaSyntax/InitializeVariables.java
@@ -0,0 +1,21 @@
+public class InitializeVariables
+{
+    public static void main(String[] args)
+    {
+        int num = 500;
+        double price = 89.25;
+        int temperature = 98;
+        int testGrade = 87;
+        double cost = 29.95;
+        
+        System.out.println(temperature);
+        System.out.println("High temperature = " + temperature);
+        System.out.println("Test grade = " + testGrade);
+        System.out.println("The cost of the shirt = " + cost);
+        
+    
+    
+    
+    
+    }
+}
