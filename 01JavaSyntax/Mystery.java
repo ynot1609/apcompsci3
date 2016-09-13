58cfc97c69acdfbaadc806ea5d23dede007f7e69
Alwin Tareen <altareen@gmail.com>
2016-09-13 15:09:19 +0800

    Added java files

---
+++ 01JavaSyntax/Mystery.java
@@ -0,0 +1,20 @@
+public class Mystery
+{
+    public static void main(String[] args)
+    {
+        int mystery = 100;
+        mystery = mystery + 50;
+        mystery--;
+        mystery = mystery * 3;
+        mystery++;
+        mystery *= 0.5;
+        mystery += 15;
+        mystery -= 7;
+        mystery++;
+        mystery -= 23;
+        mystery *= 10;
+        mystery--;
+        mystery %= 99;
+        System.out.println(mystery);
+    }
+}
