f9beaf7b5d369340acb679615a35dd947330f393
Alwin Tareen <altareen@gmail.com>
2016-09-14 08:49:41 +0800

    Updated java files

---
+++ 01JavaSyntax/Mathematics.java
@@ -0,0 +1,23 @@
+public class Mathematics
+{
+    public static void main(String[] args)
+    {
+        int num1 = 25;
+        int num2 = 6;
+        int sum = 0;
+        int difference = 0;
+        int product = 0;
+        int quotient = 0;
+        int remainder = 0;
+        
+        sum = num1 + num2;
+        difference = num1 - num2;
+        product = num1 * num2;
+        quotient = num1 / num2;
+        remainder = num1 % num2;
+        System.out.println("sum = " + sum);
+        
+    
+    
+    }
+}
