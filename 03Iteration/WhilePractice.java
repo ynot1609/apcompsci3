36259b8622398974905c702cc1ae52f49dcb9026
Alwin Tareen <altareen@gmail.com>
2016-09-20 15:09:59 +0800

    Added java files

---
+++ 03Iteration/WhilePractice.java
@@ -0,0 +1,21 @@
+public class WhilePractice
+{
+    public static void main(String[] args)
+    {
+        int count = 1;
+        while (count <= 25)
+        {
+            System.out.print(count + " ");
+            count++;
+        }
+        
+        System.out.println();
+        
+        int i = 50;
+        while (i <= 70)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
