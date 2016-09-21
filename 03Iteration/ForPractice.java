969b702bdfe4684c15d9dc610d9d116c784d1423
Alwin Tareen <altareen@gmail.com>
2016-09-21 08:45:15 +0800

    Added java files

---
+++ 03Iteration/ForPractice.java
@@ -0,0 +1,24 @@
+public class ForPractice
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i <= 25; i++)
+        {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+        
+        for (int i = 10; i >= 1; i--)
+        {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+        
+        for(int i = 1; i < 40; i += 2)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
