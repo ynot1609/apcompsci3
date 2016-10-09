f5f8c65523968b1680638082a7209753db012e5c
Alwin Tareen <altareen@gmail.com>
2016-10-09 08:56:18 +0800

    Added java files

---
+++ 05Strings/Concatenation.java
@@ -0,0 +1,10 @@
+public class Concatenation
+{
+    public static void main(String[] args)
+    {
+        String firstName = "John";
+        String lastName = "Wayne";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
---
+++ 05Strings/InstantiateString.java
@@ -0,0 +1,13 @@
+public class InstantiateString
+{
+    public static void main(String[] args)
+    {
+        String str;
+        str = "Good Morning";
+        System.out.println(str);
+        
+        // alternative
+        String greet = "Good Evening";
+        System.out.println(greet);
+    }
+}
